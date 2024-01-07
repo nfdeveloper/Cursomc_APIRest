e76dca9be9c83f6a55219126322850f92cc8ffb9
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-07 19:31:28 -0300

    Personalizando o retorno da validação

--- src/main/java/br/com/nfdeveloper/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/nfdeveloper/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -2,6 +2,8 @@ package br.com.nfdeveloper.cursomc.resources.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -22,11 +24,26 @@ public class ResourceExceptionHandler {
 	}
 	
 	// Tratador de exceções do tipo de exceção ObjectNotFoundException
-		@ExceptionHandler(DataIntegrityException.class)
-		public ResponseEntity<StandardError> dataIntegrityException(
-				DataIntegrityException e
-				,HttpServletRequest request){
-			StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrityException(
+			DataIntegrityException e
+			,HttpServletRequest request){
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(
+			MethodArgumentNotValidException e
+			,HttpServletRequest request){
+		
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",System.currentTimeMillis());
+		
+		// Para cada erro gerado na exceção será adicionado um novo erro na Classe ValidationError
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
 		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
