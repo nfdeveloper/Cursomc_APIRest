8de658fda377577873c8c720a997f8295823297c
nfdeveloper <nfdeveloper04@gmail.com>
2024-01-10 00:09:08 -0300

    Inserindo um novo cliente com POST

--- src/main/java/br/com/nfdeveloper/cursomc/resources/ClienteResources.java
+++ src/main/java/br/com/nfdeveloper/cursomc/resources/ClienteResources.java
@@ -1,5 +1,6 @@
 package br.com.nfdeveloper.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,14 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.nfdeveloper.cursomc.domain.Cliente;
 import br.com.nfdeveloper.cursomc.dto.ClienteDTO;
+import br.com.nfdeveloper.cursomc.dto.ClienteNewDTO;
 import br.com.nfdeveloper.cursomc.services.ClienteService;
 import jakarta.validation.Valid;
 
@@ -31,6 +35,18 @@ public class ClienteResources {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder
+							.fromCurrentRequest()
+							.path("/{id}")
+							.buildAndExpand(obj.getId())
+							.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@PutMapping("/{id}")
--- src/main/java/br/com/nfdeveloper/cursomc/services/ClienteService.java
+++ src/main/java/br/com/nfdeveloper/cursomc/services/ClienteService.java
@@ -10,23 +10,42 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.nfdeveloper.cursomc.domain.Cidade;
 import br.com.nfdeveloper.cursomc.domain.Cliente;
+import br.com.nfdeveloper.cursomc.domain.Endereco;
+import br.com.nfdeveloper.cursomc.domain.enums.TipoCliente;
 import br.com.nfdeveloper.cursomc.dto.ClienteDTO;
+import br.com.nfdeveloper.cursomc.dto.ClienteNewDTO;
+import br.com.nfdeveloper.cursomc.repositories.CidadeRepository;
 import br.com.nfdeveloper.cursomc.repositories.ClienteRepository;
+import br.com.nfdeveloper.cursomc.repositories.EnderecoRepository;
 import br.com.nfdeveloper.cursomc.services.exceptions.DataIntegrityException;
 import br.com.nfdeveloper.cursomc.services.exceptions.ObjectNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente buscar(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: "+ id
 				+ ", Tipo: "+ Cliente.class.getName()));
+	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -56,6 +75,21 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDTO) {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = cidadeRepository.findById(objDTO.getCidadeId()).get();
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {	
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {	
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
 	}
 	
 	// Para permanecer os dados que não foram informados no DTO.
